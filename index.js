0989338b1c85700850c474117c84e138bbbbf1d6
Sam Verschueren <sam.verschueren@gmail.com>
2015-08-04 21:52:59 +0200

    Added first draft of the service handler

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * This microservice retrieves all the selfies from the database.
+ * 
+ * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @since  4 Aug. 2015
+ */
+
+// module dependencies
+var db = require('dynongo');
+
+// connect with the database
+db.connect();
+
+var Selfie = db.table('Selfie');
+ 
+/**
+ * Main entrypoint of the service.
+ * 
+ * @param {object}  event       The data regarding the event.
+ * @param {object}  context     The AWS Lambda execution context.
+ */
+exports.handler = function(event, context) {
+    Q.fcall(function() {
+        // Retrieve all the selfies from the database
+        return Selfie.scan().exec();
+    }).then(function(result) {
+        // Send the result back to the client
+        context.succeed(result);
+    }).catch(function(err) {
+        // Print the error if something went wrong
+        console.error(err, err.stack);
+        
+        // Something went wrong
+        context.fail(err);
+    });
+};
